60da4bbe81a4b08b8ec674e9e7c6ca384c88e6ea
= <=>
2022-02-27 22:02:54 +0700

    first

---
+++ beweb/src/main/java/com/project/beweb/BewebApplication.java
@@ -0,0 +1,13 @@
+package com.project.beweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BewebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BewebApplication.class, args);
+	}
+
+}
